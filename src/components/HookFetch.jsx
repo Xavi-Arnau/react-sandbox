518b1e29f7f3667cdd499a2c4ee68c7a71a6e7e5
Xavi-Arnau <chavier28@gmail.com>
2024-09-25 18:29:01 +0200

    added a spinner

--- src/components/HookFetch.jsx
+++ src/components/HookFetch.jsx
@@ -11,11 +11,21 @@ const HookFetch = () => {
   return (
     <div>
       <h2 className="text-2xl p-4 text-white bg-blue-600">useFetchData</h2>
-      {pending ? <BeatLoader size={40} /> : null}
+      {pending ? (
+        <div className="flex justify-center">
+          <BeatLoader size={40} />
+        </div>
+      ) : null}
       {error ? <h3>{error}</h3> : null}
-      {data && data.products && data.products.length
-        ? data.products.map((item) => <p key={item.id}>{item.title}</p>)
-        : null}
+      <div className="flex flex-wrap gap-4">
+        {data && data.products && data.products.length
+          ? data.products.map((item) => (
+              <div className="w-1/5" key={item.id}>
+                {item.title}
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 };
